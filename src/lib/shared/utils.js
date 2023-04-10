d08c2cd41dda9fefcfd8972f7af7375c9ac9c720
Vinicius Bass <raylee558@gmail.com>
2023-04-10 19:25:09 +0200

    deal with browsers that don't have webworkers

--- src/lib/shared/utils.js
+++ src/lib/shared/utils.js
@@ -1,6 +1,6 @@
 // Verify if the browser supports the type property,
 // if supports we can use web workers to use multi threads on browser without any extra code
-function supportWorkerType() {
+export function supportWorkerType() {
   let supports = false
   const tester = {
     get type() { supports = true }
@@ -13,6 +13,16 @@ function supportWorkerType() {
   }
 }
 
-export {
-  supportWorkerType
+export async function loadExternalLibraries() {
+  try {
+    await Promise.all([
+      import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"),
+      import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"),
+      import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"),
+      import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"),
+    ]);
+    console.log('External libraries loaded successfully');
+  } catch (error) {
+    console.error('Failed to load external libraries', error);
+  }
 }
--- src/pages/video-player/src/factory.js
+++ src/pages/video-player/src/factory.js
@@ -1,28 +1,49 @@
 import Camera from "../../../lib/shared/camera.js"
-import { supportWorkerType } from "../../../lib/shared/utils.js"
+import { supportWorkerType, loadExternalLibraries } from "../../../lib/shared/utils.js"
 import Controller from "./controller.js"
+import Service from "./service.js"
 import View from "./view.js"
 
 async function getWorker() {
   if (supportWorkerType()) {
-    console.log('has support to workers')
+    console.log('Browser supports web workers')
     const worker = new Worker('./src/worker.js', { type: 'module' })
     return worker
   }
 
+  console.warn("Browser doesn't support web workers")
+  console.warn('Fallback to mock worker')
+
+  await import("https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js")
+  await import("https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js")
+  await import("https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js")
+  const service = new Service({
+    faceLandmarksDetection: window.faceLandmarksDetection
+  })
   const workerMock = {
-    async postMessage() {},
+    async postMessage(videoFrame) {
+      const blinked = await service.handleBlinks(videoFrame)
+      if (!blinked) return;
+      workerMock.onmessage({ data: { blinked } })
+    },
     onmessage(msg) {}
   }
 
-  console.log("browser don't support workers, mocking worker")
+  console.log('loading tf model')
+  await service.loadModel()
+  console.log('tf model loaded')
+
+  setTimeout(() => workerMock.onmessage({ data: 'TF_MODEL_READY' }), 500);
+
   return workerMock
 }
 
-const worker = await getWorker()
-const camera = await Camera.init()
 const factory = {
   async initialize() {
+    const worker = await getWorker()
+    const camera = await Camera.init()
+
     return Controller.initialize({
       view: new View(),
       worker,
